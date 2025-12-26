d6e8092d1eb315deb4d602f7a30a74d7fc74e7da
Pythonic2023 <124443031+Pythonic2023@users.noreply.github.com>
2025-12-26 12:54:10 -0500

    Added shell option to subprocess.run so we can have a shell like experience being able to cat files etc. Comments added to describe the code

--- revclientshell.py
+++ revclientshell.py
@@ -1,6 +1,8 @@
 import socket
-import time
 
+# Creates client socket, connects to server and then starts the shell loop so we can pass commands to the server to then
+# execute. We recv the message sent by the server and print it, otherwise we print no data or we have ended the connection
+# by typing break to the server.
 def shell():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('localhost', 8000))
--- revservershell.py
+++ revservershell.py
@@ -1,12 +1,17 @@
 import socket
 import subprocess
 
+
+# This function creates a server socket which listens for incoming connections. Upon connecting it shows the IP and
+# port number associated with the connection. If from the client "break" is given to the server, the server and client
+# both shutdown gracefully. You can run shell commands and get their stdout to the client when communicating with the
+# server.
 def serv():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servsock:
         servsock.bind(('localhost', 8000))
         servsock.listen(3)
         (sock, addr_info) = servsock.accept()
-        print(f'connection from: {addr_info[0]} {addr_info[1]}')
+        print(f'connection from: {addr_info[0]} {addr_info[1]}') # print the IP and port of connection
         while True:
             client_message = sock.recv(1024).decode('utf-8')
             if client_message == 'break':
@@ -14,8 +19,20 @@ def serv():
                 sock.send(closing_message.encode('utf-8'))
                 break
             else:
-                result = subprocess.run(client_message, capture_output=True)
-                sock.send(result.stdout)
+                execute_command(client_message, sock)
+
+# When you pass a command that is something other than break, this function will be called and the result will be passed
+# To the client socket communicating with the server. pass our client message/command and sock to execute_function.
+def execute_command(command, sock):
+    # pass the shell=True so we can execute shell commands the same as we would with a shell, otherwise doing "cat file"
+    # would fail and would return 'cat file' file or directory not found. capture output to have both STDOUT and STDERR
+    # captured.
+    result = subprocess.run(command, shell=True, capture_output=True)
+    # Send stderr is return code not 0, otherwise send stdout back to the client.
+    if result.returncode != 0:
+        sock.send(result.stderr)
+    else:
+        sock.send(result.stdout)
 
 
 if __name__ == '__main__':
